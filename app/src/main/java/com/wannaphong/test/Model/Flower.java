f39a246fd66b311f65250f388c672212f155ce17
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-05 22:49:39 +0700

    Add listitem and Flower

---
+++ app/src/main/java/com/wannaphong/test/Model/Flower.java
@@ -0,0 +1,4 @@
+package com.wannaphong.test.Model;
+
+public class Flower {
+}
---
+++ app/src/main/java/com/wannaphong/test/listitem_flower.kt
@@ -0,0 +1,12 @@
+package com.wannaphong.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class listitem_flower : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listitem_flower)
+    }
+}
