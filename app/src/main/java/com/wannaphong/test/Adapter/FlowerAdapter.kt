05cabd87b34b358caffe98a8446c7b5e9e1cf418
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-05 23:06:33 +0700

    Update FlowerAdapter

--- app/src/main/java/com/wannaphong/test/Adapter/FlowerAdapter.kt
+++ app/src/main/java/com/wannaphong/test/Adapter/FlowerAdapter.kt
@@ -1,4 +1,37 @@
 package com.wannaphong.test.Adapter
 
-class FlowerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.wannaphong.test.Model.Flower
+import com.wannaphong.test.R
+
+class FlowerAdapter(val mCtx: Context,
+                    var resource:Int,
+                    var items:List<Flower>)
+    : ArrayAdapter<Flower>(mCtx,resource,items){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        //return super.getView(position, convertView, parent)
+        val layout: LayoutInflater = LayoutInflater.from(mCtx)
+
+        val v: View = layout.inflate(resource,null)
+        val icon : ImageView = v.findViewById(R.id.imageView)
+        val name : TextView = v.findViewById(R.id.textView)
+
+        val flower:Flower = items[position]
+
+        name.text = flower.name
+        try {
+            icon.setImageDrawable(mCtx.getDrawable(flower.icon))
+        }
+        catch (ex:Exception){
+
+        }
+
+        return v
+    }
 }
--- app/src/main/java/com/wannaphong/test/MainActivity.kt
+++ app/src/main/java/com/wannaphong/test/MainActivity.kt
@@ -2,11 +2,16 @@ package com.wannaphong.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wannaphong.test.Model.Flower
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var flower:MutableList<Flower> = mutableListOf<Flower>()
+        flower.add(Flower("ดอกดาวเรือง",R.mipmap.ic_launcher))
+        flower.add(Flower("ดอกกล้วยไม้",R.mipmap.ic_launcher))
     }
 }
